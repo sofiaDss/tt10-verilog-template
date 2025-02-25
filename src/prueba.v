2b873d901fe96d52322c8f53b836958f655630a5
sofiaDss <119266255+sofiaDss@users.noreply.github.com>
2025-02-25 08:01:58 +0100

    Update prueba.v

--- src/prueba.v
+++ src/prueba.v
@@ -1,13 +1,16 @@
 `default_nettype none
 
 module tt_um_fulladder_sofiaDss (
-	input wire a,
+	/*input wire a,
 	input wire b,
 	input wire cin,
 	output wire s,
-	output wire count
+	output wire count*/
+	input  wire [7:0] ui_in,    // Dedicated inputs
+    	output wire [7:0] uo_out   // Dedicated outputs
 );
 
-assign s = a ^ b ^ cin;
-assign count = a & b | a & cin | b & cin;
+/*assign s = a ^ b ^ cin;
+assign count = a & b | a & cin | b & cin;*/
+	assign uo_out =ui_in;
 endmodule
