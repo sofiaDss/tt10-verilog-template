e9baa418eef80605e11662b33348993d69cc1991
sofiaDss <119266255+sofiaDss@users.noreply.github.com>
2025-03-19 15:12:50 +0100

    Update prueba.v

--- src/prueba.v
+++ src/prueba.v
@@ -8,8 +8,10 @@ module tt_um_prueba (
     //output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       en1,      // always 1 when the design is powered, so you can ignore it
     input  wire       en2,      // clock
-    output wire       en_out
-    //input  wire       rst_n     // reset_n - low to reset
+    output wire       en_out,
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
   // All output pins must be assigned. If not used, assign to 0.
@@ -18,6 +20,6 @@ module tt_um_prueba (
   //assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  //wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
